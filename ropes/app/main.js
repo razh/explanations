6e1474c07fa1181769ab199f04a34698804d2636
Raymond Zhou <somecrazyidiot@gmail.com>
2013-06-15 00:34:57 -0400

    Boilerplate JS and CSS for ropes project.

---
+++ ropes/app/main.js
@@ -0,0 +1,29 @@
+requirejs.config({
+  shim: {
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: [ 'jquery', 'underscore' ],
+      exports: 'Backbone'
+    }
+  },
+
+  paths: {
+    'backbone': 'lib/backbone/backbone',
+    'jquery': 'lib/jquery/jquery-2.0.2',
+    'underscore': 'lib/underscore/underscore'
+  }
+});
+
+
+define(
+  [ 'jquery',
+    'underscore',
+    'backbone' ],
+  function( $, _, Backbone ) {
+    console.log( $ );
+    console.log( _ );
+    console.log( Backbone );
+  }
+);
