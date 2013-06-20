22dcc9bfb13ae7729a11a357d00ad44c2601ce22
Raymond Zhou <somecrazyidiot@gmail.com>
2013-06-20 16:22:22 -0400

    Add red-black tree. Refactor out requirejs config to top-level.

--- rope/app/main.js
+++ rope/app/main.js
@@ -1,26 +1,3 @@
-requirejs.config({
-  shim: {
-    'underscore': {
-      exports: '_'
-    },
-    'backbone': {
-      deps: [ 'jquery', 'underscore' ],
-      exports: 'Backbone'
-    },
-    'd3': {
-      exports: 'd3'
-    }
-  },
-
-  paths: {
-    'backbone': '../../lib/backbone/backbone',
-    'd3': '../../lib/d3/d3.v3',
-    'jquery': '../../lib/jquery/jquery-2.0.2',
-    'underscore': '../../lib/underscore/underscore'
-  }
-});
-
-
 define(
   [ 'jquery',
     'underscore',
