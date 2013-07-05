93a69e6adb99e466c0cf0a4f85713a0e0ff373b7
Raymond Zhou <somecrazyidiot@gmail.com>
2013-07-05 00:05:52 -0400

    Add basics for fibonacci heaps.

---
+++ app/fibonacci-heap/models/fib-heap-node.js
@@ -0,0 +1,12 @@
+define(
+  [ 'shared/models/node' ],
+  function( Node ) {
+    'use strict';
+
+    var FibHeapNode = Node.extend({
+
+    });
+
+    return FibHeapNode;
+  }
+);
---
+++ app/fibonacci-heap/models/fib-heap.js
@@ -0,0 +1,12 @@
+define(
+  [ 'backbone',
+    'fibonacci-heap/models/fib-heap-node' ],
+  function( Backbone, FibHeapNode ) {
+    'use strict';
+
+    var FibHeap = Backbone.Model.extend({
+    });
+
+    return FibHeap;
+  }
+);
